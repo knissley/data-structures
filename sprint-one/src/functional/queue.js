cc5569ddba3eb729830f494856b6954581199ea2
Kyle Nissley <knissley97@gmail.com>
2022-02-24 12:40:06 -0800

    Pass tests for functional implementation of the stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    //this is adding to the queue, which happens in the back of the queue
+
+    //check if this is the first value added
+    if (Object.keys(storage).length === 0) {
+      storage['0'] = value;
+    } else {
+      //all
+    }
   };
 
   someInstance.dequeue = function() {
+    //this is removing from the queue, which happens from the front of the queue
   };
 
   someInstance.size = function() {
+    //return the size of the queue
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,45 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  //size
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    //SMARTER WAY (thanks Sean!)
+
+    //utilize the new size property to keep track of how long the stack is
+    storage[size] = value;
+    size++;
+
+    //MY NAIVE WAY
+
+    // //try to keep track of the size of the stack
+    // //check if there is nothing in storage
+    // if (Object.keys(storage).length === 0) {
+    //   // if so the current value needs to be pushed to the first value
+    //   storage['0'] = value;
+    // } else {
+    //   //else need to calculate the new number to use for the key
+    //   //alias the keys array
+    //   let keysArray = Object.keys(storage);
+    //   //take the most recent value in the keys and add one for the new key
+    //   let newKey = keysArray[keysArray.length - 1] + 1;
+    //   storage[newKey] = value;
+    // }
   };
 
   someInstance.pop = function() {
+    if (size > 0) {
+      let lastValue = storage[size - 1];
+      delete storage[size];
+      size--;
+      return lastValue;
+    }
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
